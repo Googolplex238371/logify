9188222c648355fb70ef3a765293f96813106833
Googolplex238371 <111164474+Googolplex238371@users.noreply.github.com>
2024-09-12 21:53:18 +0800

    Create animation.js

---
+++ website/static/animation.js
@@ -0,0 +1,6 @@
+window.onload = function(){wifi = document.getElementById("wifi").value
+ban = document.getElementById("ban").value
+document.getElementById("content").innerHTML += '<div id="main" style = "display:none"><div id="wifi"> <h1 class = "fa">'+wifi+'</h1> </div><div id="ban"> <h1 class = "fa" style="color :red">'+ban+'</h1> </div></div><style>#main {position:relative; z-index: 1;  height: 400px; width:400px;   } #wifi {font-size:100px;position:absolute; z-index: -1; height: 80px; width:200px;   } #ban {font-size:100px;z-index: -1;position: absolute;left:16px;opacity:0}</style>'
+}
+a = 0
+setInterval(function(){if(window.navigator.onLine){document.getElementById("main").style.display="block";document.getElementById("ban").style.opacity = Math.abs(Math.sin(a)).toString();a+=Math.PI/135}},10)
